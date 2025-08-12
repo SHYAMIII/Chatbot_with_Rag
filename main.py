366dcbe4a08a20d455732591e6116a052ed37c92
shyamCoder <112856141+SHYAMIII@users.noreply.github.com>
2025-08-12 22:14:16 +0530

    Update main.pyd

--- main.py
+++ main.py
@@ -31,5 +31,6 @@ async def chat(request: ChatRequest):
 
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn, os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
